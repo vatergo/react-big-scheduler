b0dfb5faf1256991a193aac7a83332e3bb2d1a3c
vatergo <vatergo@mail.ru>
2024-05-22 17:32:34 +0300

    fix: add alias for react-dnd

--- example/withDnDContext.js
+++ example/withDnDContext.js
@@ -1,4 +1,4 @@
-import {DragDropContext} from 'react-dnd'
-import HTML5Backend from 'react-dnd-html5-backend'
+import { DragDropContext } from "react-dnd-scheduler";
+import HTML5Backend from "react-dnd-html5-backend-scheduler";
 
 export default DragDropContext(HTML5Backend);
